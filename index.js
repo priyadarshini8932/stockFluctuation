72a56057990947bccec7855f624cfb71f7cd5470
priyadarshini8932 <shreyapriyadarshini032@gmail.com>
2026-05-30 12:33:52 +0530

    new-images-added

--- index.js
+++ index.js
@@ -22,19 +22,24 @@ function updatePage(stockData){
     stockDisplaySymbol.innerText = sym
     stockDisplayPrice.innerText = price
     stockDisplayTime.innerText = time
-    let icon
+    let icon=document.getElementById("price-icon")
+
     if(prevPrice===null){
+        icon.src="./images/triangle-equal.png"
         icon='➡'
     }
     else if(prevPrice>price){
+        icon.src="./images/triangle-down.png"
         icon='⬇'
     }
     else if(prevPrice<price){
+        icon.src="./images/triangle-up.png"
         icon='⬆'
     }
     else{
+        icon.src="./images/triangle-equal.png"
         icon='➡'
     }
     prevPrice=price
-    stockDisplayPriceIcon.innerText=icon
+    
 }
